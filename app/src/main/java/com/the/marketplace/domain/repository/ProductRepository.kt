d6d951b42ca48b3fb2b5004ce1065dee65306753
Ara Simonyan <simonyanara35@gmail.com>
2026-03-12 06:08:45 -0700

    Update ProductRepository.kt

--- app/src/main/java/com/the/marketplace/domain/repository/ProductRepository.kt
+++ app/src/main/java/com/the/marketplace/domain/repository/ProductRepository.kt
@@ -4,8 +4,4 @@ import com.the.marketplace.domain.model.Product
 
 interface ProductRepository {
     suspend fun getProducts(query: String): List<Product>
-
-//    suspend fun getProductById(id: Int): Product
-
-
 }
